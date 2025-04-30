932fac3a9c818b46a804d74ba107ffc3812f02da
Mitesh Ashara <asharamitesh007@gmail.com>
2025-04-30 15:46:53 +0530

    admin theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { ServicesComponent } from './pages/services/services.component';
+import { SuppliersComponent } from './pages/home/suppliers/suppliers.component';
+import { CustomersComponent } from './pages/home/customers/customers.component';
+import { OrdersComponent } from './pages/home/orders/orders.component';
+
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'services', component: ServicesComponent },
-    { path: 'contact', component: ContactComponent },
+    { path: 'suppliers', component: SuppliersComponent },
+    { path: 'customers', component: CustomersComponent },
+    { path: 'orders', component: OrdersComponent },
 ];
